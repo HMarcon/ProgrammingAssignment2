728735364145abb80eae2f0cfcc5cdda8f78b38f
HMarcon <henriquemmagri42@gmail.com>
2020-06-19 14:40:24 -0300

    Creation of makeCacheMatrix. Adapted from vector example

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Using functions to cache the result of a matrix inversion. These calculations
+## can be very time-consumming and it grows to the n^3, where n is the size of
+## the matrix.
+
+##  cacheMatrix$set(M)      # Change the matrix being cached.
+##  M <- cacheMatrix$get()  # Returns the matrix being cached.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        cachedInverse <- NULL
+        set <- function(y) {
+                x <<- y
+                cachedInverse <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) cachedInverse <<- inverse
+        getInverse <- function() cachedInverse
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
